2c6dd0ce0973e861a29573c88073ffa36bc9ff84
kid <1051716213@qq.com>
2019-07-17 19:39:48 +0800

    发布部分

--- src/views/publish/publish.vue
+++ src/views/publish/publish.vue
@@ -1,13 +1,53 @@
 <template>
-    <img width="800" src="http://uploads.5068.com/allimg/160119/60-1601191SA4-51.jpg" alt="">
+    <div class="container">
+        <el-card>
+            <div slot="header">
+                <my-bread>发布文章</my-bread>
+            </div>
+            <el-form :v-model="articleForm">
+                <el-form-item label="标题: ">
+                    <el-input style="width:400px"></el-input>
+                </el-form-item>
+                <el-form-item label="内容: ">
+                        富文本
+                </el-form-item>
+                <el-form-item label="封面: ">
+                        <el-radio-group v-model="articleForm.cover.type">
+                            <el-radio :label="1">单图</el-radio>
+                            <el-radio :label="3">三图</el-radio>
+                            <el-radio :label="0">无图</el-radio>
+                            <el-radio :label="-1">自动</el-radio>
+                        </el-radio-group>
+                        <div class="img-btn"><img src="../../assets/images/default.png" alt=""></div>
+                </el-form-item>
+                <el-form-item label="频道: ">
+                        <my-channel v-model="articleForm.channel_id"></my-channel>
+                </el-form-item>
+            </el-form>
+        </el-card>
+    </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      articleForm: {
+        title: '',
+        content: '',
+        cover: {
+          type: 1,
+          imges: ''
+        },
+        channel_id: null
+      }
+    }
+  }
 }
 </script>
 
 <style>
-
+.img-btn{
+    width: 180px;
+}
 </style>
