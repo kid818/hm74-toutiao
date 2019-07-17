56cbe300fedc590a46a0d03491db3031f2e487f5
kid <1051716213@qq.com>
2019-07-17 08:41:32 +0800

    欢迎页图片居中

--- src/views/welcome/welcome.vue
+++ src/views/welcome/welcome.vue
@@ -1,13 +1,19 @@
 <template>
-    <img width="800" src="https://cn.bing.com/th?id=OIP.YhqkqgdJFIg0K6yQ11cxLgHaEo&pid=Api&rs=1&p=0" alt="">
+  <div class="container">
+    <img
+      width="800"
+      src="https://cn.bing.com/th?id=OIP.YhqkqgdJFIg0K6yQ11cxLgHaEo&pid=Api&rs=1&p=0"
+      alt
+    />
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style scoped lang="less">
-
+.container {
+  text-align: center;
+}
 </style>
